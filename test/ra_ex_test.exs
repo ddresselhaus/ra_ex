fce0a904d2aba16c0bf7b7753458849e7013f6e7
Daniel Dresselhaus <ddresselhaus@gmail.com>
2018-11-17 00:42:54 -0500

    Add ex_unit_clustered_case

--- test/ra_ex_test.exs
+++ test/ra_ex_test.exs
@@ -1,8 +1,19 @@
 defmodule RaExTest do
-  use ExUnit.Case
-  doctest RaEx
+  use ExUnit.ClusteredCase, async: false
 
-  test "greets the world" do
-    assert RaEx.hello() == :world
+  scenario "given a healthy cluster", cluster_size: 3 do
+    # Node setups work similar to `setup` in ExUnit, but are run on each node of the cluster
+    # node_setup([:start_apps, :seed_kvstore])
+    node_setup(:config_node)
+
+    # Just plain old tests - note the :cluster key of the context, which is needed to talk
+    # to the nodes of the cluster via the Cluster API (an alias added for you)
+    test "writes are replicated to all nodes", %{cluster: cluster} do
+      [_a, _b, _c] = Cluster.members(cluster)
+    end
+  end
+
+  def config_node(_) do
+    Application.put_env(:ex_unit_clustered_case, :name, Node.self())
   end
 end
