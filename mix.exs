c1ada27a41d53eeea098f3cf16176ad41bd61733
Daniel Dresselhaus <ddresselhaus@gmail.com>
2018-12-18 19:48:37 -0500

    Update ra ref

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule RaEx.MixProject do
   defp deps do
     [
       {:ex_unit_clustered_case, "~> 0.3.2"},
-      {:ra, git: "https://github.com/rabbitmq/ra"}
+      {:ra, git: "https://github.com/rabbitmq/ra", ref: "b6a0d626"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
