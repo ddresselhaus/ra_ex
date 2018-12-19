aa300beb614a493df9ee57b193d580ae82096fcd
Daniel Dresselhaus <ddresselhaus@gmail.com>
2018-12-18 20:48:35 -0500

    Make clustered_case optional

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule RaEx.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_unit_clustered_case, "~> 0.3.2"},
+      {:ex_unit_clustered_case, "~> 0.3.2", optional: true},
       {:ra, git: "https://github.com/rabbitmq/ra", ref: "b6a0d626"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
